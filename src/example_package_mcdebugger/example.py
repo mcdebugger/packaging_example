eb5eb9f3f80bdf255329e02ef852c92120969711
Maksim Kachur <me@maksimkachur.ru>
2025-03-11 21:58:47 +0300

    Add main() function for CLI tool

--- src/example_package_mcdebugger/example.py
+++ src/example_package_mcdebugger/example.py
@@ -3,3 +3,14 @@ def add_one(number):
 
 def add_two(number):
     return number + 2
+
+def main():
+    digit = 1
+    
+    print(f'Executing add_one calculation on digit: {digit}')
+    digit = add_one(digit)
+    print(f'Result: {digit}')
+    
+    print(f'Executing add_two calculation on digit: {digit}')
+    digit = add_two(digit)
+    print(f'Result: {digit}')
