7f03cc2f945365cc5c5eaacdd7934b560e918eaf
Maksim Kachur <me@maksimkachur.ru>
2025-03-12 01:47:23 +0300

    Some updates and useful additions

--- src/example_package_mcdebugger/example.py
+++ src/example_package_mcdebugger/example.py
@@ -14,3 +14,5 @@ def main():
     print(f'Executing add_two calculation on digit: {digit}')
     digit = add_two(digit)
     print(f'Result: {digit}')
+    
+    print('Congratulations! You\'re computing magician!')
